b5ea5cb8e96ed97ac0ebd8a5a6ecf941e8b37f33
PC-SUN\Administrator <wlcsunchuanwei@163.com>
2017-02-20 13:37:11 +0800

    更改微信授权方式

--- Applications/Chat/Web/callback.php
+++ Applications/Chat/Web/callback.php
@@ -14,45 +14,8 @@ curl_setopt($ch, CURLOPT_URL, $url);
 $json =  curl_exec($ch);
 curl_close($ch);
 $arr=json_decode($json,1);
-if(empty($arr['access_token']) || empty($arr['openid']))
-{
-//header("Location: http://".$_SERVER['SERVER_NAME']); 
-//特殊情况没有获取到用户数据 给个默认值
-$name = '游客';
-$headimgurl = "http://".$_SERVER['SERVER_NAME']."/img/moren.jpg";
-$YS = "问者";
-$strPol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
-$max = strlen($strPol)-1;
-for($i=0;$i<29;$i++){
-    $str.=$strPol[rand(0,$max)];//rand($min,$max)生成介于min和max两个数之间的一个随机整数
- }
-$openid = $str;
-header("Location: http://".$_SERVER['SERVER_NAME']."/client.php?name=".urlencode($name)."&headimgurl=".urlencode($headimgurl)."&YS=".urlencode($YS)."&openid=".urlencode($openid)."&room_id=1"); 
-exit();
-}
-else
-{
-$access_token = $arr['access_token'];
-$openid = $arr['openid'];	
-}
-$url='https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_URL, $url);
-$json =  curl_exec($ch);
-curl_close($ch);
-$arr=json_decode($json,1);
-if(empty($arr['nickname']) || empty($arr['headimgurl']))
-{
-//header("Location: http://".$_SERVER['SERVER_NAME']); 
-exit('没有获取到用户名');
-}
-else
-{
 $name=$arr['nickname']; //用户名
 $headimgurl=$arr['headimgurl']; //头像
-}
 include("./config.php");
 //查询数据库 有值标志 医生
 $sql = "select * from yisheng where openid = '".$openid."' ";
--- Applications/Chat/Web/index.php
+++ Applications/Chat/Web/index.php
@@ -1,14 +1,13 @@
 <?php
-header("Location: http://127.0.0.1/client.php?name=%E6%9C%88%E5%A4%9C%E5%BF%A7%E4%BC%A4&headimgurl=http%3A%2F%2Fwx.qlogo.cn%2Fmmopen%2FajNVdqHZLLDibMX1fr8Z2GF9XMnwtn7xYB1JYrjy7NmvDehSSA0BnJ5Mgx5rCY3HnMHTyUFva4ZMj8tVaRDPBiag%2F0&YS=%E9%97%AE%E8%80%85&openid=oRbpWt0bD1rcikuNEaAr-OFzMirI&room_id=1");
-
-// header("Content-type: text/html; charset=utf-8"); 
-// session_start();
-// $AppID="wxa1340e0924412e4e";
-// $AppSecret = '2838bf068c8466cac720d85723e983e4';
-// $callback  =  urlencode('http://s-210917.abc188.com/callback.php'); //回调地址
-// $ip = $_SERVER['REMOTE_ADDR'];
-// $time = time();
-// $state = md5($time.$ip);
-// $_SESSION["wx_state"] = $state; //存到SESSION
-// header("Location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$AppID."&redirect_uri=".$callback."&response_type=code&scope=snsapi_base&state=".$state."#wechat_redirect"); 
+//header("Location: http://127.0.0.1/client.php?name=%E6%9C%88%E5%A4%9C%E5%BF%A7%E4%BC%A4&headimgurl=http%3A%2F%2Fwx.qlogo.cn%2Fmmopen%2FajNVdqHZLLDibMX1fr8Z2GF9XMnwtn7xYB1JYrjy7NmvDehSSA0BnJ5Mgx5rCY3HnMHTyUFva4ZMj8tVaRDPBiag%2F0&YS=%E9%97%AE%E8%80%85&openid=oRbpWt0bD1rcikuNEaAr-OFzMirI&room_id=1");
+header("Content-type: text/html; charset=utf-8"); 
+session_start();
+$AppID="wxa1340e0924412e4e";
+$AppSecret = '2838bf068c8466cac720d85723e983e4';
+$callback  =  urlencode('http://s-210917.abc188.com/callback.php'); //回调地址
+$ip = $_SERVER['REMOTE_ADDR'];
+$time = time();
+$state = md5($time.$ip);
+$_SESSION["wx_state"] = $state; //存到SESSION
+header("Location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$AppID."&redirect_uri=".$callback."&response_type=code&scope=snsapi_userinfo&state=".$state."#wechat_redirect"); 
 ?>
